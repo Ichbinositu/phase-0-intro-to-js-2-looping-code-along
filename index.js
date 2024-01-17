d686c167418ab49fd607e3232f1a86c1afdf97a5
Ichbinositu <hesbon.ezekiel@student.moringaschool.com>
2024-01-17 13:52:12 +0300

    Done the looping lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGift(gift) {
+  console.log(`Wrapped ${gift} and added a bow!`);
+}
+basic structure => for ([initialization]; [condition]; [iteration]) {
+    [loop body]
+}
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+const names = [];
+
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    names[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return names;
+}
+writeCards(names);
+
+
+function countDown(num) {
+  let i = 0
+  while (i <= num) {
+    console.log(num--)
+  }
+}
